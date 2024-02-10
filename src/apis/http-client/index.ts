26054cbdf8f0b9773b35445e47a313b3438eb308
Seung-wan <tmddhks0104@gmail.com>
2024-02-10 13:50:04 +0900

    msw prod에서도 동작하도록 수정

--- src/apis/http-client/index.ts
+++ src/apis/http-client/index.ts
@@ -1,5 +1,5 @@
 import axios, { type AxiosInstance } from 'axios';
 
 export const httpClient: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:5173',
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5173',
 });
